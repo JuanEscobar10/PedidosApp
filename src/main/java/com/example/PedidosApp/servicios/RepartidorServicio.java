819d5e590d9c802df0357ead1a402799a10e456f
JuanEscobar10 <juanescobardavid2007@hotmail.com>
2025-04-10 20:31:49 -0500

    se crean los servicios para repartidor y tienda

--- src/main/java/com/example/PedidosApp/servicios/RepartidorServicio.java
+++ src/main/java/com/example/PedidosApp/servicios/RepartidorServicio.java
@@ -9,4 +9,6 @@ public class RepartidorServicio {
 
     @Autowired
     IRepartidorRepositorio repositorio;
+    
+    
 }
--- src/main/java/com/example/PedidosApp/servicios/TiendaServicio.java
+++ src/main/java/com/example/PedidosApp/servicios/TiendaServicio.java
@@ -9,5 +9,5 @@ public class TiendaServicio {
 
     @Autowired
     ITiendaRepositorio repositorio;
-    
+
 }
